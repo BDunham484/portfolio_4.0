81471fd292d1f855b57c18d70cc6e01074b0902a
ThereWolf <bdunham484@gmail.com>
2025-01-19 18:13:44 -0600

    Flesh out the Header a bit

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -1,8 +1,34 @@
-const Header = () => {
-    return (
-        <div>
+'use client';
+import { useCallback, useState } from 'react';
+import { VscChromeClose, VscMenu } from 'react-icons/vsc';
 
-        </div>
+
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+    const hamburgerIcon = <VscMenu id='hamburger' onClick={() => setIsMenuOpen(prevState => !prevState)} />;
+    const closeIcon = <VscChromeClose id='closeHamburger' onClick={() => setIsMenuOpen(prevState => !prevState)} />;
+    const closeMobileMenu = useCallback(() => setIsMenuOpen(false), []);
+    const animateFrom = { opacity: 0, y: -80 };
+    const animateTo = { opacity: 1, y: 0 };
+
+    return (
+        <header>
+            <div>
+                <h1>
+                    <a href="#">
+                        Brad Dunham
+                    </a>
+                </h1>
+                <p>
+                    Dev
+                </p>
+            </div>
+            <nav>
+                <ul>
+                    
+                </ul>
+            </nav>
+        </header>
     );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-// import Image from 'next/image';
-// import styles from './page.module.css';
-
 import Header from './Components/Header/Header';
-
 
 export default function Home() {
 
