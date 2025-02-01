07b7be5425d87d7dd412af6dc09c1ad0be2bc8e7
ThereWolf <bdunham484@gmail.com>
2025-02-01 16:24:00 -0600

    Update links

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -32,9 +32,9 @@ const Header = () => {
             </div>
             <nav>
                 <ul>
-                    <li><Link href='#'>Home</Link></li>
                     <li><Link href='#'>About</Link></li>
                     <li><Link href='#'>Projects</Link></li>
+                    <li><Link href='#'>Resume</Link></li>
                     <li><Link href='#'>Contact</Link></li>
                 </ul>
             </nav>
