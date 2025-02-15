13b6375312433a64df029b2ee7bef0ae9aaa0492
ThereWolf <bdunham484@gmail.com>
2025-02-15 15:12:56 -0600

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['www.freeiconspng.com'], // add any external domains you use
+  },
+
 };
 
 export default nextConfig;
